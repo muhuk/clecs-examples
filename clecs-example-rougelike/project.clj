24125997f83c4fe3aa9afdc8bc6797a2a0cf47fc
Atamert Ölçgen <muhuk@muhuk.com>
2015-01-07 17:12:46 +0200

    Upgraded dependencies.

--- clecs-example-rougelike/project.clj
+++ clecs-example-rougelike/project.clj
@@ -1,13 +1,13 @@
 (defproject clecs-example-rougelike "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple rouge-like game written using clecs."
+  :url "https://github.com/muhuk/clecs-examples"
   :license {:name "GNU GPL v3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clecs "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+                 [clecs "1.1.0-SNAPSHOT"]
                  [clojure-lanterna "0.9.4"]
-                 [com.stuartsierra/component "0.2.1"]]
+                 [com.stuartsierra/component "0.2.2"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.8"]
                                   [org.clojure/java.classpath "0.2.2"]]}}
   :main clecs-example-rougelike.core)
